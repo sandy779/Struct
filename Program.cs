24cb3a9ac1c2afb412157590917a0bfdf71b4f83
sandip ahire <sandipahire34@gmail.com>
2021-09-06 22:02:33 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Struct
+{
+    public struct Student
+    {
+        public int StudentId;
+        public int RgistrationId;
+        public string Studname;
+        public string course;
+
+    }
+    
+   
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Student s1;
+            Console.WriteLine("Enter StudentId:");
+            s1.StudentId =Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine("Enter Registrationid:");
+            s1.RgistrationId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Student Name:");
+            s1.Studname = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter Course in which you want to Enroll:");
+            s1.course = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("==========================================================");
+            Console.WriteLine(s1.StudentId);
+            Console.WriteLine(s1.RgistrationId);
+            Console.WriteLine(s1.Studname);
+            Console.WriteLine(s1.course);
+
+
+        }
+    }
+}
